da2c6fd9b73f9af86f2cbcc05fce00319049a24c
Gropp <fgropp@gmail.com>
2022-03-19 13:19:29 -0300

    primeiro commit

---
+++ js/owl/setupowl.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    // efeito de loop
+    loop:false,
+    // margin entre as imagens
+    margin:10,
+    // botoes de navegacao
+    nav:false,
+    // tela pequena 1 item, 600px 3 itens, 1000px 5 itens por tela
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
